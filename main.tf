623d5d6b8ee9740970805152fa21324e276da648
huifeng <huifeng@pinduoduo.com>
2023-02-02 21:03:52 +0800

    createazmachine

--- main.tf
+++ main.tf
@@ -5,32 +5,32 @@ data "azurerm_resource_group" "rg" {
 data "azurerm_subnet" "internal" {
   name                 = var.private_subnet_name
   virtual_network_name = var.virtual_network_name
-  resource_group_name  = azurerm_resource_group.rg.name
+  resource_group_name  = data.azurerm_resource_group.rg.name
 }
 
 resource "azurerm_network_interface" "main" {
   name                = "${var.prefix}-nic"
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
+  location            = data.zurerm_resource_group.rg.location
+  resource_group_name = data.azurerm_resource_group.rg.name
 
   ip_configuration {
     name                          = "${var.prefix}-main-ip"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = data.azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
     primary = true
   }
 
    ip_configuration {
     name                          = "${var.prefix}-seconnd-ip"
-    subnet_id                     = azurerm_subnet.internal.id
+    subnet_id                     = data.azurerm_subnet.internal.id
     private_ip_address_allocation = "Dynamic"
   }
 }
 
 resource "azurerm_virtual_machine" "main" {
   name                  = "${var.prefix}-vm"
-  location              = azurerm_resource_group.rg.location
-  resource_group_name   = azurerm_resource_group.rg.name
+  location              = data.azurerm_resource_group.rg.location
+  resource_group_name   = data.azurerm_resource_group.rg.name
   network_interface_ids = [azurerm_network_interface.main.id]
   vm_size               = var.vm_size
   delete_os_disk_on_termination = true
